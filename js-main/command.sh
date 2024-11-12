b15bc6df2ea4b13d773697089304210cfcd7ac40
jskim12345 <wltjr08740@gmail.com>
2024-11-12 13:14:02 +0900

    1300

--- js-main/command.sh
+++
@@ -1,5 +0,0 @@
-python train.py --data-file=daily_2019_kp_all.csv --graph-type=VAE;
-python train.py --data-file=daily_2019_kp_all.csv --graph-type=MHA;
-python train.py --data-file=daily_2019_kp_all.csv --graph-type=Dense;
-python train.py --data-file=daily_2019_kp_all.csv --graph-type=Transition;
-python train.py --data-file=daily_2019_kp_all.csv --graph-type=PAM;
